bc042b9d78e1d380831dbe3efe21d64485b62c83
yakuphantonintshi <yakupha58756@gmail.com>
2024-05-24 16:09:44 +0200

    Add CSS

--- scripts/code.js
+++ scripts/code.js
@@ -7,18 +7,26 @@ function Submit() {
   let idNumb = document.querySelector(".id").value;
   let output = document.querySelector(".output");
   let results = document.querySelector(".results");
-  console.log(output)
-//   console.log(typeof idNumb)
-  if (idNumb.length == 13) {
-    output.innerHTML = `Date of birth: ${idNumb.slice(4, 6)}/${idNumb.slice(2,4)}/20${idNumb.slice(0, 2)}`;
+
+    console.log(idNumb.length);
+  if (idNumb.length != 13 || idNumb.slice(4 , 6) > 31 || idNumb.slice(2 , 4) > 12 ){
+    output.innerText = `ID number not valid`;
+    results.innerText = ""
+   
+   } else {
+    if ((idNumb.length == 13) && (idNumb[0] >-1 && idNumb[0] < 3))  {
+        output.innerHTML = `Date of birth: ${idNumb.slice(4, 6)}/${idNumb.slice(2,4)}/20${idNumb.slice(0, 2)}`;
+      } 
+      else{
+        output.innerHTML =`Date of birth: ${idNumb.slice(4, 6)}/${idNumb.slice(2,4)}/19${idNumb.slice(0, 2)}`;
+    }
+
     if(idNumb.slice(6, 10) > 5000){
         results.innerHTML = `Gender: Male`;
-    }
-    else{
+    }else{
         results.innerHTML = "Gender: Female"
     }
+   }
 }
-else{
-    output.innerHTML = "ID number not valid";
-}
-}
+
+
